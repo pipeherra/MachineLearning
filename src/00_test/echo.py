467626bb4e0206d11bfe85b15aa936fccdbb2187
Andre Plueth <andre.plueth@fh-muenster.de>
2020-04-22 23:36:32 +0200

    00_Test_Echo

---
+++ src/00_test/echo.py
@@ -0,0 +1,14 @@
+from iul.iul import IUL
+
+iul = IUL()
+exercise_id = "echo"
+data = iul.get_data(exercise_id)
+
+data_session = data['session']
+data_value = data['value']
+
+result = {
+    'session': data_session,
+    'result': data_value,
+}
+iul.post_result(exercise_id, result)
---
+++ src/iul/iul.py
@@ -0,0 +1,50 @@
+from pprint import pprint
+
+import requests
+import json
+
+from iul.config import Config
+
+
+class IUL:
+    def __init__(self, config=Config()):
+        self.config = config
+
+    def get_data(self, excercise_id):
+        data_url = self.config.host + "/exercises/" + excercise_id + "/data"
+        try:
+            response = requests.get(data_url, headers={'Authorization': 'Token ' + self.config.token})
+            if response.status_code != 200:
+                print("Could not get data from IUL-server")
+                exit(1)
+            json_data = json.loads(response.text)
+            print("\nReceived exercise from server\n-------------------------------")
+            pprint(json_data)
+            print("-------------------------------\n\n")
+            return json_data
+        except Exception as ex:
+            print("Exception: Could not get data from IUL-Server")
+            print(ex)
+            exit(1)
+        return ""
+
+    def post_result(self, excercise_id, result):
+        print("Calculated result\n-------------------------------")
+        pprint(result)
+        print("-------------------------------\n\n")
+        result_url = self.config.host + "/exercises/" + excercise_id + "/result"
+        try:
+            response = requests.post(result_url, json=result, headers={'Authorization': 'Token ' + self.config.token, 'content-type': 'application/json'})
+            if response.status_code != 200:
+                print("Could not post result to IUL-server")
+                exit(1)
+            json_classification = json.loads(response.text)
+            print("\nReceived classification from server\n-------------------------------")
+            pprint(json_classification)
+            print("-------------------------------\n")
+            return json_classification
+        except Exception as ex:
+            print("Exception: Could not post result to IUL-Server")
+            print(ex)
+            exit(1)
+        return ""
