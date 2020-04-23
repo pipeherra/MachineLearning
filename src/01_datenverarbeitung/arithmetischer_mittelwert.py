40b6eed85ee599fe2390e1b122ba676c314e4dcb
Andre Plueth <andre.plueth@fh-muenster.de>
2020-04-23 13:56:46 +0200

    00_Datenverarbeitung_Arithmetischer-Mittelwert

---
+++ src/01_datenverarbeitung/arithmetischer_mittelwert.py
@@ -0,0 +1,29 @@
+from iul.iul import IUL
+
+iul = IUL(False)
+exercise_id = "mean"
+data = iul.get_data(exercise_id)
+
+data_session = data["session"]
+data_signals = data["signals"]
+signal_results = []
+
+for data_signal in data_signals:
+    signal_id = data_signal["id"]
+    signal_values = data_signal["values"]
+    mean = 0.0
+    for signal_value in signal_values:
+        mean += signal_value
+    mean /= len(signal_values)
+
+    signal_result = {
+        'id': signal_id,
+        'mean': mean
+    }
+    signal_results.append(signal_result)
+result = {
+    'session': data_session,
+    'results': signal_results
+}
+
+iul.post_result(exercise_id, result)
