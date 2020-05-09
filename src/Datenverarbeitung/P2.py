a713f76b89ee22c751c42382c3f02b41d8fe4248
ah108929 <andresf.herrerau@gmail.com>
2020-05-09 13:59:11 +0200

    Perceptron

--- src/Datenverarbeitung/P2.py
+++ src/Datenverarbeitung/P2.py
@@ -2,7 +2,18 @@ import pandas as pd
 
 from src.signals.statistics import Statistics
 
-data = pd.read_csv('../../data/19SoSe/2019_daten_grp2/ruhelage.csv')
+data = pd.read_csv('C:/Users/Andres/meuml/data/17SoSe/2017_Gruppe6_Appelfeller-Krupa/gehen.csv')
+
+columns = list(data)
+
+for column in columns:
+    if column in ['ID', 'Timestamp']:
+        continue
+    values = data[column].to_numpy()
+    print("{}: Mittelwert: {}, Median: {}, Standardabweichung: {}".format(column, Statistics.get_mean(values),
+          Statistics.get_median(values), Statistics.get_standard_deviation(values)))
+
+data = pd.read_csv('C:/Users/Andres/meuml/data/17SoSe/2017_Gruppe6_Appelfeller-Krupa/ruhe.csv')
 
 columns = list(data)
 
