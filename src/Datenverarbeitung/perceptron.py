03d237ab37dc1c9de848019f5b09cad896c05826
ah108929 <andresf.herrerau@gmail.com>
2020-05-11 15:29:05 +0200

    Perceptron training

--- src/Datenverarbeitung/perceptron.py
+++ src/Datenverarbeitung/perceptron.py
@@ -1,15 +1,8 @@
 import math
+import numpy as np
 
 
 class Perceptron:
-    # Default constructor for a perceptron
-    # def __init__(self):
-    # self.w = -1 + np.random.rand(features + 1) * 2  # Initialize weights randomly on range [-1:1]
-    # self.maxEpochs = maxEpochs  # Max iterations
-    # self.learning_rate = learning_rate  # Learning rate
-
-    # def trin(self):
-    #    pass
 
     def predict_sum(self, data_weights, data_inputs):
         threshold = data_weights.pop(0)
@@ -24,3 +17,30 @@ class Perceptron:
     def predict_with_normalized_tan(self, data_weights, data_inputs):
         data_sum = self.predict_sum(data_weights, data_inputs)
         return (math.tanh(data_sum) * 0.5) + 0.5
+
+    def predict_sum_th(self, data_weights, data_inputs, threshold):
+        if len(data_weights) != len(data_inputs):
+            print("Len not equal")
+            return
+        data_sum = threshold
+        for (data_weight, data_input) in zip(data_weights, data_inputs):
+            data_sum += data_weight * data_input
+        return data_sum
+
+    def predict_with_normalized_tan_th(self, data_weights, data_inputs, threshold):
+        data_sum = self.predict_sum_th(data_weights, data_inputs, threshold)
+        return (math.tanh(data_sum) * 0.5) + 0.5
+
+    def train(self, data_weights, data_inputs, data_class, learning_rate, threshold):
+        iterations = 1000
+
+        for iteration in range(1, iterations):
+
+            prediction = self.predict_with_normalized_tan_th(data_weights, data_inputs, threshold)
+            error = (data_class - prediction)
+
+            if prediction != data_class:
+                for i in range(1, len(data_weights)):
+                    data_weights[i] += learning_rate * error * data_inputs[i]
+
+        return data_weights
--- src/Datenverarbeitung/perzeptron_training.py
+++ src/Datenverarbeitung/perzeptron_training.py
@@ -2,25 +2,33 @@ from src.Datenverarbeitung.perceptron import Perceptron
 from src.iul.iul import IUL
 
 iul = IUL(False)
-exercise_id = "perceptron_processing"
+exercise_id = "perceptron_training"
 request_data = iul.get_data(exercise_id)
 
 request_data_session = request_data["session"]
 attribute_count = request_data["attribute-count"]
-data_array = request_data["data"]
+learning_rate = request_data["learning-rate"]
+data_weights = request_data["initial-weights"]
+data_array = request_data["training-data"]
 
 perceptron = Perceptron()
+
+threshold = data_weights.pop(0)
+
+print(data_weights)
+weights = data_weights[1]
+print(weights)
 
 results = []
 for data in data_array:
     data_id = data["id"]
-    data_weights = data["weights"]
     data_inputs = data["input"]
-    data_value = perceptron.predict_with_normalized_tan(data_weights, data_inputs)
-
+    data_class = data['class']
+    data_value = perceptron.train(data_weights, data_inputs, data_class, learning_rate, threshold)
+    data_weights = data_value
     data_result = {
         'id': data_id,
-        'value': data_value
+        'weights': data_value
     }
     results.append(data_result)
 
