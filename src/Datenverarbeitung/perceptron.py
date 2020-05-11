d7667750d84871b2e6ece1a9c9791676aa721dd3
Andre Plueth <andre.plueth@fh-muenster.de>
2020-05-11 12:14:42 +0200

    moved to class

--- src/Datenverarbeitung/perceptron.py
+++ src/Datenverarbeitung/perceptron.py
@@ -1,26 +1,26 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import math
 
 
 class Perceptron:
-
-    # tanh Transfger Function
-    def activation(self, x):
-        t = (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
-        return t
-
     # Default constructor for a perceptron
-    def __init__(self, maxEpochs, learning_rate, features):
-        self.w = -1 + np.random.rand(features + 1) * 2  # Initialize weights randomly on range [-1:1]
-        self.maxEpochs = maxEpochs  # Max iterations
-        self.learning_rate = learning_rate  # Learning rate
+    # def __init__(self):
+    # self.w = -1 + np.random.rand(features + 1) * 2  # Initialize weights randomly on range [-1:1]
+    # self.maxEpochs = maxEpochs  # Max iterations
+    # self.learning_rate = learning_rate  # Learning rate
 
-    # Function to provide a guess for a specific input
-    def guess(self, input):
-        sum = 0
-        for i in range(0, len(self.w)):
-            sum += self.w[i] * input[i]
-        return self.activation(sum)
+    # def trin(self):
+    #    pass
 
-    def trin(self):
-        pass
+    def predict_sum(self, data_weights, data_inputs):
+        threshold = data_weights.pop(0)
+        if len(data_weights) != len(data_inputs):
+            print("Len not equal")
+            return
+        data_sum = threshold
+        for (data_weight, data_input) in zip(data_weights, data_inputs):
+            data_sum += data_weight * data_input
+        return data_sum
+
+    def predict_with_normalized_tan(self, data_weights, data_inputs):
+        data_sum = self.predict_sum(data_weights, data_inputs)
+        return (math.tanh(data_sum) * 0.5) + 0.5
--- src/Datenverarbeitung/perzeptron_verarbeitung.py
+++ src/Datenverarbeitung/perzeptron_verarbeitung.py
@@ -1,8 +1,5 @@
-import math
-
 from src.Datenverarbeitung.perceptron import Perceptron
 from src.iul.iul import IUL
-import numpy as np
 
 iul = IUL(False)
 exercise_id = "perceptron_processing"
@@ -12,24 +9,17 @@ request_data_session = request_data["session"]
 attribute_count = request_data["attribute-count"]
 data_array = request_data["data"]
 
+perceptron = Perceptron()
+
 results = []
 for data in data_array:
     data_id = data["id"]
     data_weights = data["weights"]
-    data_threshold = data_weights.pop(0)
     data_inputs = data["input"]
-
-    data_sum = data_threshold
-    for (data_weight, data_input) in zip(data_weights, data_inputs):
-        data_sum += data_weight * data_input
-    data_value = math.tanh(data_sum)
-    data_value_norm = (data_value * 0.5) + 0.5
-    print("ID: {}, Threshold: {}, Weights-Len: {}, Inputs-Len:{}, Data-Sum: {}, Data-Value: {}, Data-Value-Norm: {}"
-          .format(data_id, data_threshold, len(data_weights), len(data_inputs), data_sum, data_value, data_value_norm))
-
+    data_value = perceptron.predict_with_normalized_tan(data_weights, data_inputs)
     data_result = {
         'id': data_id,
-        'value': data_value_norm
+        'value': data_value
     }
     results.append(data_result)
 
